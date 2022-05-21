827f330b3a1a92ca0d70e01df8004f15a471df9d
Sai Teja <saitejamadireddy@gmail.com>
2022-05-20 21:00:59 -0400

    intial changes

---
+++ build/precache-manifest.94109cf2762cfcd32655d361a5533ed6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3fd86dae50017e73ccb9fe55784eb27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "682570754ddadeadb94a",
+    "url": "/static/css/main.2836148e.chunk.css"
+  },
+  {
+    "revision": "2b33d2a8552739fd2f3d",
+    "url": "/static/js/2.b24121f2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b24121f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "682570754ddadeadb94a",
+    "url": "/static/js/main.5f0eaf6c.chunk.js"
+  },
+  {
+    "revision": "2814220894937123fbdd",
+    "url": "/static/js/runtime-main.37440f35.js"
+  },
+  {
+    "revision": "06092ce752fd0cd6be631db3a0e99ce9",
+    "url": "/static/media/devIcon.06092ce7.svg"
+  },
+  {
+    "revision": "93ff54375c4849247c1b67c2bb5d9856",
+    "url": "/static/media/me.93ff5437.png"
+  }
+]);
